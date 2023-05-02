20947d6ad3f1385b3a5e35af2d471cf1af69832e
Spectre <pgudijan@uci.edu>
2023-05-02 16:49:11 -0700

    state

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import ReactDOM from 'react-dom'
+import GuestList from './state/GuestList'
 
 const App = () => {
     return (
     <div>
-        hello
+        <GuestList />
     </div>
     )
 };
